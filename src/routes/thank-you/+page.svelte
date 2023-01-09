379ac356ca8a020c340c8ba7916000e8024f2879
wilfredo-flores <willflores1221@gmail.com>
2023-01-09 18:03:19 -0500

    Add page

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,22 @@
+<div class="thanks">
+	Form submitted. We'll get back to you soon. <a href="/">Return to Home Page.</a>
+</div>
+
+<style>
+	.thanks {
+		padding-top: 6rem;
+		font-size: 1.5rem;
+		height: 100vh;
+	}
+
+	@media screen and (max-width: 992px) {
+		.thanks {
+			font-size: 2.6rem;
+		}
+	}
+	@media screen and (max-width: 500px) {
+		.thanks {
+			font-size: 2.2rem;
+		}
+	}
+</style>

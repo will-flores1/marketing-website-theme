f96d39bc1044d3622e14563e69ff74b5efd6913b
wilfredo-flores <willflores1221@gmail.com>
2023-01-09 18:04:31 -0500

    Change headline

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,10 +3,10 @@
 
 <section class="wrap">
 	<div class="hero">
-		<h1>Launch your dreams today.</h1>
-		<p>Stop wrestling with website builders</p>
+		<h1>Wrestling with website builders?</h1>
+		<p>Launch your website today.</p>
 		<a href="/contact">
-			<input class="button-1" type="button" value="get started" />
+			<input class="button-1" type="button" value="Get in touch" />
 		</a>
 	</div>
 	<div />
@@ -37,21 +37,21 @@
 		animation: fadeIn 0.5s;
 	}
 	p {
-		font-size: clamp(1.8rem, 3vw, 1rem);
-		font-weight: 300;
-		padding: 1rem 0 0 0;
+		font-size: clamp(1.8rem, 3.5vw, 2rem);
+		font-weight: 400;
+		padding: 1.8rem 0 0 0;
 		color: var(--color-text-1);
 		animation: fadeIn 0.5s;
 	}
 
 	.button-1 {
-		margin-top: 3rem;
+		margin-top: 1.9rem;
 		padding: 1rem 3rem;
 		font-size: clamp(1.4rem, 1.5vw, 9rem);
 		max-width: 17rem;
-		background-color: rgba(240, 248, 255, 0);
+		background-color: rgb(246, 25, 128);
 		border: 2px solid rgb(246, 25, 128);
-		color: rgb(246, 25, 128);
+		color: white;
 		border-radius: 4px;
 		animation: fadeIn 0.5s;
 		transition: all 0.3s ease;
@@ -59,7 +59,7 @@
 
 	.button-1:hover {
 		cursor: pointer;
-		background-color: rgb(246, 25, 128);
+		background-color: rgb(211, 0, 99);
 		color: white;
 	}
 

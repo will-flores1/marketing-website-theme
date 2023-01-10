f96fc5406380505facc5c9da021aaeb12f79693e
wilfredo-flores <willflores1221@gmail.com>
2023-01-10 09:27:24 -0500

    Add icons

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,9 +1,29 @@
+<script>
+	import WebDevelopment from '../lib/images/webdevelopment.svg';
+	import Globe from '../lib/images/globe.svg';
+	import Marketing from '../lib/images/marketing.svg';
+	import Tools from '../lib/images/tools.svg';
+</script>
+
 <div class="service-wrap">
 	<h1 class="what-we-do">WHAT WE DO</h1>
 	<div class="cell-container">
 		<div class="cell">
 			<div class="text-wrap">
-				<h1 class="service-title">Custom website development</h1>
+				<img src={Globe} width="48" alt="Globe" />
+
+				<h1 class="service-title">Website Development</h1>
+				<p>
+					Website design & development. You don't just want a website that works, you want a website
+					that builds an excellent customer experience that keeps them coming back.
+				</p>
+			</div>
+		</div>
+		<div class="cell">
+			<div class="text-wrap">
+				<img src={WebDevelopment} width="48" alt="Web Development" />
+
+				<h1 class="service-title">Scale & Growth</h1>
 				<p>
 					Back-end website development. You need a website that helps you scale and outgrow your
 					competition.
@@ -13,15 +33,8 @@
 		</div>
 		<div class="cell">
 			<div class="text-wrap">
-				<h1 class="service-title">Website Presence</h1>
-				<p>
-					Website design & development. You don't just want a website that works, you want a website
-					that builds an excellent customer experience that keeps them coming back.
-				</p>
-			</div>
-		</div>
-		<div class="cell">
-			<div class="text-wrap">
+				<img src={Marketing} width="48" alt="marketing" />
+
 				<h1 class="service-title">Online Marketing</h1>
 				<p>
 					SEO optimization & content writing. You don’t want to be included in just any search, you
@@ -31,6 +44,7 @@
 		</div>
 		<div class="cell">
 			<div class="text-wrap">
+				<img src={Tools} width="48" alt="tools" />
 				<h1 class="service-title">Other Services</h1>
 				<p>
 					Website maintenance and google service analytics. Ensure your website remains up-to-date,

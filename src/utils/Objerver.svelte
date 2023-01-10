f16a0b931c79bd7b4e9b923da14e9068b65fd4d3
wilfredo-flores <willflores1221@gmail.com>
2023-01-10 12:45:22 -0500

    Add IO api

---
+++ src/utils/Objerver.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let observer: any = null;
+
+	onMount(() => {
+		observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				console.log(entry);
+				if (entry.isIntersecting) {
+					entry.target.classList.add('shown');
+
+					// observer.disconnect();
+				}
+				// else {
+				// 	entry.target.classList.remove('shown');
+				// }
+			});
+		});
+
+		const hiddenElements = document.querySelectorAll('.hidden');
+		hiddenElements.forEach((el) => {
+			observer.observe(el);
+		});
+	});
+</script>
+
+<div>
+	<slot />
+</div>

aa23d7f8678de0b0b312cc5b15f429a14ea359d7
wilfredo-flores <willflores1221@gmail.com>
2023-01-10 12:44:06 -0500

    Hover svg & title

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,68 +1,198 @@
 <script>
-	import WebDevelopment from '../lib/images/webdevelopment.svg';
-	import Globe from '../lib/images/globe.svg';
-	import Marketing from '../lib/images/marketing.svg';
-	import Tools from '../lib/images/tools.svg';
+	import Objerver from '../utils/Objerver.svelte';
+	let width = 55;
 </script>
 
-<div class="service-wrap">
-	<h1 class="what-we-do">WHAT WE DO</h1>
-	<div class="cell-container">
-		<div class="cell">
-			<div class="text-wrap">
-				<img src={Globe} width="48" alt="Globe" />
+<Objerver>
+	<div class="service-wrap">
+		<h1 class="what-we-do hidden">WHAT WE DO</h1>
+		<div class="cell-container">
+			<div class="cell hidden">
+				<div class="text-wrap">
+					<div class="hover-service">
+						<svg
+							version="1.1"
+							id="Icons"
+							xmlns="http://www.w3.org/2000/svg"
+							{width}
+							x="0px"
+							y="0px"
+							viewBox="0 0 32 32"
+						>
+							<line class="st1" x1="3" y1="11" x2="29" y2="11" />
+							<line class="st1" x1="5" y1="8" x2="7" y2="8" />
+							<line class="st1" x1="8" y1="8" x2="10" y2="8" />
+							<line class="st1" x1="11" y1="8" x2="13" y2="8" />
+							<path
+								class="st1"
+								d="M8.8,27H3V5h26v22l-5.8,0c0.3-0.1,0.6-0.1,0.9-0.1c0.1-0.6,0.2-1.3,0.2-1.9c0-0.7-0.1-1.3-0.2-1.9
+		c-1,0.1-2-0.3-2.5-1.3c-0.5-0.9-0.4-2,0.2-2.8c-0.9-0.9-2.1-1.6-3.4-2c-0.4,0.9-1.3,1.6-2.4,1.6s-2-0.7-2.4-1.6
+		c-1.3,0.4-2.4,1.1-3.4,2c0.6,0.8,0.7,1.9,0.2,2.8c-0.5,0.9-1.6,1.4-2.5,1.3c-0.1,0.6-0.2,1.3-0.2,1.9c0,0.7,0.1,1.3,0.2,1.9
+		C8.2,26.9,8.5,26.9,8.8,27L8.8,27z"
+							/>
+							<circle class="st0" cx="16" cy="25" r="3" />
+						</svg>
 
-				<h1 class="service-title">Website Development</h1>
-				<p>
-					Website design & development. You don't just want a website that works, you want a website
-					that builds an excellent customer experience that keeps them coming back.
-				</p>
+						<h1 class="service-title">Website Development</h1>
+					</div>
+					<p>
+						Website design & development. You don't just want a website that works, you want a
+						website that builds an excellent customer experience that keeps them coming back.
+					</p>
+				</div>
 			</div>
-		</div>
-		<div class="cell">
-			<div class="text-wrap">
-				<img src={WebDevelopment} width="48" alt="Web Development" />
+			<div class="cell hidden">
+				<div class="text-wrap">
+					<div class="hover-service">
+						<svg
+							version="1.1"
+							id="Icons"
+							xmlns="http://www.w3.org/2000/svg"
+							{width}
+							x="0px"
+							y="0px"
+							viewBox="0 0 32 32"
+						>
+							<line class="st1" x1="3" y1="11" x2="29" y2="11" />
+							<line class="st1" x1="5" y1="8" x2="7" y2="8" />
+							<line class="st1" x1="8" y1="8" x2="10" y2="8" />
+							<line class="st1" x1="11" y1="8" x2="13" y2="8" />
+							<path
+								class="st1"
+								d="M8.8,27H3V5h26v22l-5.8,0c0.3-0.1,0.6-0.1,0.9-0.1c0.1-0.6,0.2-1.3,0.2-1.9c0-0.7-0.1-1.3-0.2-1.9
+					 c-1,0.1-2-0.3-2.5-1.3c-0.5-0.9-0.4-2,0.2-2.8c-0.9-0.9-2.1-1.6-3.4-2c-0.4,0.9-1.3,1.6-2.4,1.6s-2-0.7-2.4-1.6
+					 c-1.3,0.4-2.4,1.1-3.4,2c0.6,0.8,0.7,1.9,0.2,2.8c-0.5,0.9-1.6,1.4-2.5,1.3c-0.1,0.6-0.2,1.3-0.2,1.9c0,0.7,0.1,1.3,0.2,1.9
+					 C8.2,26.9,8.5,26.9,8.8,27L8.8,27z"
+							/>
+							<circle class="st0" cx="16" cy="25" r="3" />
+						</svg>
 
-				<h1 class="service-title">Scale & Growth</h1>
-				<p>
-					Back-end website development. You need a website that helps you scale and outgrow your
-					competition.
-				</p>
+						<h1 class="service-title">Scale & Growth</h1>
+					</div>
+					<p>
+						Back-end website development. You need a website that helps you scale and outgrow your
+						competition.
+					</p>
+				</div>
+				<p />
 			</div>
-			<p />
-		</div>
-		<div class="cell">
-			<div class="text-wrap">
-				<img src={Marketing} width="48" alt="marketing" />
+			<div class="cell hidden">
+				<div class="text-wrap">
+					<div class="hover-service">
+						<svg
+							{width}
+							xmlns="http://www.w3.org/2000/svg"
+							x="0px"
+							y="0px"
+							viewBox="0 0 247.065 247.065"
+						>
+							<g>
+								<path
+									d="M160.551,36.935c-0.614-1.892-1.956-3.462-3.727-4.365c-1.774-0.904-3.831-1.065-5.724-0.45l-31.376,10.196
+							 c-3.939,1.28-6.096,5.511-4.815,9.45l1.568,4.828l-79.811,58.625L5.183,125.448c-1.892,0.615-3.462,1.956-4.365,3.728
+							 c-0.903,1.772-1.065,3.831-0.45,5.723l19.129,58.862c1.03,3.169,3.97,5.184,7.131,5.184c0.769,0,1.55-0.119,2.32-0.369
+							 l31.478-10.229l16.173,0.085c3.248,15.336,16.888,26.88,33.176,26.88c16.164,0,29.714-11.371,33.095-26.531l16.587,0.087
+							 l1.568,4.829c0.614,1.892,1.955,3.462,3.728,4.365c1.064,0.542,2.232,0.817,3.405,0.817c0.78,0,1.563-0.122,2.317-0.367
+							 l31.377-10.195c3.939-1.28,6.096-5.511,4.816-9.45L160.551,36.935z M31.444,181.992l-14.492-44.597l18.364-5.967l14.49,44.597
+							 L31.444,181.992z M109.774,200.312c-7.912,0-14.694-4.887-17.513-11.797l34.958,0.184
+							 C124.356,195.514,117.617,200.312,109.774,200.312z M64.714,173.369l-7.888-24.277l-7.888-24.277l72.419-53.194l22.209,68.349
+							 l11.006,33.873L64.714,173.369z M172.972,181.929l-0.921-2.833c-0.001-0.005-0.002-0.011-0.004-0.017l-19.815-60.983l-20.74-63.833
+							 l17.111-5.561l41.48,127.665L172.972,181.929z"
+								/>
+								<path
+									d="M185.807,73.393c1.092,0.556,2.254,0.819,3.4,0.819c2.73,0,5.363-1.496,6.688-4.096l13.461-26.41
+							 c1.882-3.69,0.415-8.207-3.275-10.088c-3.69-1.88-8.207-0.415-10.088,3.276l-13.461,26.41
+							 C180.65,66.995,182.117,71.512,185.807,73.393z"
+								/>
+								<path
+									d="M242.176,144.712l-26.414-13.455c-3.691-1.879-8.207-0.412-10.087,3.279c-1.881,3.691-0.412,8.207,3.278,10.087
+							 l26.414,13.455c1.091,0.555,2.253,0.818,3.398,0.818c2.73,0,5.364-1.497,6.689-4.097
+							 C247.335,151.109,245.867,146.593,242.176,144.712z"
+								/>
+								<path
+									d="M204.242,101.204c1.03,3.169,3.97,5.184,7.131,5.184c0.769,0,1.55-0.119,2.32-0.369l28.188-9.16
+							 c3.938-1.28,6.095-5.511,4.814-9.451c-1.28-3.94-5.511-6.092-9.451-4.815l-28.188,9.16
+							 C205.118,93.034,202.962,97.265,204.242,101.204z"
+								/>
+							</g>
+						</svg>
+						<h1 class="service-title">Online Marketing</h1>
+					</div>
+					<p>
+						SEO optimization & content writing. You don’t want to be included in just any search,
+						you want to be noticed by the potential customers that matter most.
+					</p>
+				</div>
+			</div>
+			<div class="cell hidden">
+				<div class="text-wrap">
+					<div class="hover-service">
+						<svg xmlns="http://www.w3.org/2000/svg" {width} x="0px" y="0px" viewBox="0 0 32 32">
+							<style type="text/css">
+								.st0 {
+									fill: none;
+									stroke: inherit;
+									stroke-width: 2;
+									stroke-linecap: round;
+									stroke-linejoin: round;
+									stroke-miterlimit: 10;
+								}
+								.st1 {
+									fill: none;
+									stroke: inherit;
+									stroke-width: 2;
+									stroke-linejoin: round;
+									stroke-miterlimit: 10;
+								}
+								.st2 {
+									fill: none;
+									stroke: inherit;
+									stroke-width: 2;
+									stroke-linecap: round;
+									stroke-miterlimit: 10;
+								}
+							</style>
+							<path
+								class="st0"
+								d="M27.8,28.2L27.8,28.2c-1.3,1.3-3.3,1.3-4.6,0L17,22l4.6-4.6l6.2,6.2C29.1,24.8,29.1,26.9,27.8,28.2z"
+							/>
+							<polygon
+								class="st0"
+								points="6.2,3.5 3.2,6.6 5.5,10.5 9.3,12.8 17.8,21.3 20.9,18.2 12.4,9.7 10.1,5.8 "
+							/>
+							<path
+								class="st1"
+								d="M18.9,16.1l2.7-2.7c1.9,0.7,4.2,0.4,5.7-1.2c1.5-1.5,1.9-3.8,1.2-5.7l-2.6,2.6l-2.4-0.6l-0.6-2.4l2.6-2.6
+		c-1.9-0.7-4.2-0.4-5.7,1.2c-1.5,1.5-1.9,3.8-1.2,5.7l-2.7,2.7"
+							/>
+							<path
+								class="st1"
+								d="M12.7,16.3l-2.3,2.3c-1.9-0.7-4.2-0.4-5.7,1.2c-1.5,1.5-1.9,3.8-1.2,5.7l2.6-2.6l2.4,0.6l0.6,2.4l-2.6,2.6
+		c1.9,0.7,4.2,0.4,5.7-1.2c1.5-1.5,1.9-3.8,1.2-5.7l2.4-2.4"
+							/>
+						</svg>
 
-				<h1 class="service-title">Online Marketing</h1>
-				<p>
-					SEO optimization & content writing. You don’t want to be included in just any search, you
-					want to be noticed by the potential customers that matter most.
-				</p>
-			</div>
-		</div>
-		<div class="cell">
-			<div class="text-wrap">
-				<img src={Tools} width="48" alt="tools" />
-				<h1 class="service-title">Other Services</h1>
-				<p>
-					Website maintenance and google service analytics. Ensure your website remains up-to-date,
-					secure, and healthy.
-				</p>
+						<h1 class="service-title">Other Services</h1>
+					</div>
+					<p>
+						Website maintenance and google service analytics. Ensure your website remains
+						up-to-date, secure, and healthy.
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</Objerver>
 
 <style>
 	.what-we-do {
 		text-align: start;
-		font-size: 2rem;
+		font-size: 7.5rem;
 		letter-spacing: -1px;
-		padding: 3rem 0 0 2.344rem;
+		padding: 3rem 3rem 0 3rem;
 	}
 	.service-wrap {
+		display: flex;
 		font-family: 'Inter', sans-serif;
 		height: fit-content;
 		letter-spacing: -1px;
@@ -103,6 +233,18 @@
 		/* width: max-content; */
 	}
 
+	.hover-service {
+		fill: black;
+		stroke: black;
+		transition: all 0.3s ease;
+	}
+
+	.hover-service:hover {
+		color: var(--color-theme-1);
+		fill: var(--color-theme-1);
+		stroke: var(--color-theme-1);
+	}
+
 	@media screen and (max-width: 992px) {
 		.cell-container {
 			padding: 2.344rem;
@@ -128,6 +270,14 @@
 		}
 		.cell {
 			width: 100%;
+		}
+	}
+	@media screen and (max-width: 992px) {
+		.what-we-do {
+			font-size: 4rem;
+		}
+		.service-wrap {
+			display: block;
 		}
 	}
 </style>
